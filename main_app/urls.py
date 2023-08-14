7c15c75cc4602d563180a2204bdd22bbc29baa6b
whuang214 <whuang2146@gmail.com>
2023-08-14 16:48:37 -0400

    added bootstrap styles

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path # import path function from django.urls
-from . import views # importing views.py from main_app
+from django.urls import path  # import path function from django.urls
+from . import views  # importing views.py from main_app
 
 # all routes for this app
 urlpatterns = [
-    path('', views.home, name='home'), # setting the route for the home page
+    path("", views.home, name="home"),  # setting the route for the home page
+    path("about/", views.about, name="about"),  # setting the route for the about page
+    path(
+        "finches/", views.finches_index, name="finches"
+    ),  # setting the route for the all finches page
 ]
-
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+
 # home controller
 def home(request):
     # format is render(request, template_name, context)
     # request is the request object
     # template_name is the path to the template
     # context is optional (data you want to pass to the template)
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def finches_index(request):
+    # filler
+    return render(request, "finches/index.html")
