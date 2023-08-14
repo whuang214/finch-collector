783d105f3c35223d45ab2ee2c0cac580166bd210
whuang214 <whuang2146@gmail.com>
2023-08-14 18:29:34 -0400

    add more fields on fitch and detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.home, name="home"),  # setting the route for the home page
     path("about/", views.about, name="about"),  # setting the route for the about page
     path(
-        "finches/", views.finches_index, name="finches"
+        "finches/", views.finch_index, name="finches"
     ),  # setting the route for the all finches page
+    path("finches/<int:finch_id>/", views.finch_detail, name="finch_detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 
+# Filler Finch Data
+finches = [
+    {
+        "name": "Zebra Finch",
+        "description": "The zebra finch is a small bird...",
+        "image_url": "https://via.placeholder.com/150",
+        "id": 1,
+        "scientific_name": "Taeniopygia guttata",
+        "average_lifespan": "5-7 years",
+        "native_region": "Australia, Indonesia, East Timor",
+    },
+    {
+        "name": "Gouldian Finch",
+        "description": "The gouldian finch is known for its vibrant colors...",
+        "image_url": "https://via.placeholder.com/150",
+        "id": 2,
+        "scientific_name": "Erythrura gouldiae",
+        "average_lifespan": "6-8 years",
+        "native_region": "Northern Australia",
+    },
+]
 
-# home controller
+
 def home(request):
-    # format is render(request, template_name, context)
-    # request is the request object
-    # template_name is the path to the template
-    # context is optional (data you want to pass to the template)
     return render(request, "home.html")
 
 
@@ -14,6 +31,12 @@ def about(request):
     return render(request, "about.html")
 
 
-def finches_index(request):
-    # filler
-    return render(request, "finches/index.html")
+def finch_index(request):
+    context = {"finches": finches}
+    return render(request, "finches/index.html", context)
+
+
+def finch_detail(request, finch_id):
+    finch = finches[finch_id - 1]
+    context = {"finch": finch}
+    return render(request, "finches/detail.html", context)
