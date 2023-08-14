eea004f45dc6eaf825b224500b6298f1a6edee31
whuang214 <whuang2146@gmail.com>
2023-08-14 18:51:51 -0400

    migrated finch and added database data

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,5 @@
 from django.shortcuts import render
-
-# Filler Finch Data
-finches = [
-    {
-        "name": "Zebra Finch",
-        "description": "The zebra finch is a small bird...",
-        "image_url": "https://via.placeholder.com/150",
-        "id": 1,
-        "scientific_name": "Taeniopygia guttata",
-        "average_lifespan": "5-7 years",
-        "native_region": "Australia, Indonesia, East Timor",
-    },
-    {
-        "name": "Gouldian Finch",
-        "description": "The gouldian finch is known for its vibrant colors...",
-        "image_url": "https://via.placeholder.com/150",
-        "id": 2,
-        "scientific_name": "Erythrura gouldiae",
-        "average_lifespan": "6-8 years",
-        "native_region": "Northern Australia",
-    },
-]
+from .models import Finch
 
 
 def home(request):
@@ -32,11 +11,11 @@ def about(request):
 
 
 def finch_index(request):
-    context = {"finches": finches}
-    return render(request, "finches/index.html", context)
+    # get the data from the database
+    fitches = Finch.objects.all()
+    return render(request, "finches/index.html", {"finches": fitches})
 
 
 def finch_detail(request, finch_id):
-    finch = finches[finch_id - 1]
-    context = {"finch": finch}
-    return render(request, "finches/detail.html", context)
+    fitch = Finch.objects.get(id=finch_id)
+    return render(request, "finches/detail.html", {"finch": fitch})
