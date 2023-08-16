6152e748c67eb6551465db4f49d1147338e1332b
whuang214 <whuang2146@gmail.com>
2023-08-16 15:11:52 -0400

    change home page and about page text

--- main_app/models.py
+++ main_app/models.py
@@ -4,11 +4,8 @@ from django.urls import reverse
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
-    scientific_name = models.CharField(max_length=100)
     description = models.TextField()
     image_url = models.URLField()
-    average_lifespan = models.PositiveIntegerField()
-    native_region = models.CharField(max_length=100)
 
     # This is the string representation of the model
     # if someone did print(finch) it would return the name
