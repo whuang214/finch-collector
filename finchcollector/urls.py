310f60285bb03c17d907fc19c8def78dc357f1b7
whuang214 <whuang2146@gmail.com>
2023-08-17 13:53:00 -0400

    add account urls to finch collector

--- finchcollector/urls.py
+++ finchcollector/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("main_app.urls")), # setting the root route
+    path("admin/", admin.site.urls),
+    path("", include("main_app.urls")),  # setting the root route
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
