3fb58b219125be8dd7e1d41bb611af9baa144fba
whuang214 <whuang2146@gmail.com>
2023-08-17 11:35:58 -0400

    add user model

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,14 @@ MEALS = (
 )
 
 
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+
 class Food(models.Model):
     name = models.CharField(max_length=50)
 
